7e3279499aa94f8f4104c05e677b4df3da626382
IniOlu <agbeleyeinioluwa@gmail.com>
2026-07-05 05:06:27 +0100

    Add project files.

---
+++ DOMAIN/Tweet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tweet_Audit.DOMAIN
+{
+    public class Tweet
+    {
+        public int Id { get; set; }
+        public string FullText { get; set; }
+        public string CreatedAt { get; set; }
+        public string RetweetCount { get; set; }
+        public string FavoriteCount { get; set; }
+    }
+}
