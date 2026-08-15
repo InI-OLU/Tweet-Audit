6f462a6d115e6848dec91804f65453d09797126a
IniOlu <agbeleyeinioluwa@gmail.com>
2026-08-15 10:34:42 +0100

    feat(PromptBuilder): I created the PromptBuilder class

--- INFRASTRUCTURE/ArchiveParser.cs
+++ INFRASTRUCTURE/ArchiveParser.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -10,13 +12,23 @@ namespace Tweet_Audit.INFRASTRUCTURE
 {
     public class ArchiveParser
     {
+        private readonly ArchiveTweetPathSettings filepath;
 
+        public ArchiveParser(IOptions<ArchiveTweetPathSettings> settings)
+        {
+            filepath = settings.Value;
+        }
        
         public List<Tweet> ArchiveReadAndParse()
         {
-            using FileStream tweetFile = File.OpenRead();
-            jsonD
-            List<Tweet> tweets = JsonSerializer.Deserialize<List<Tweet>>(json);
+            string rawContent = File.ReadAllText(filepath.ArchivePath);
+
+            int jsonStartIndex = rawContent.IndexOf('[');
+            string json = rawContent.Substring(jsonStartIndex);
+
+            List<Tweet> tweets = JsonSerializer.Deserialize<List<Tweet>>(json)
+                ?? new List<Tweet>();
+
             return tweets;
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Tweet_Audit.INFRASTRUCTURE;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.Configure<ArchiveTweetPathSettings>(builder.Configuration.GetSection("ArchiveSettings"));
+
+using IHost host = builder.Build();
